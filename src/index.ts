81a1787fdc7c54826129291b486aee2841e960d9
joaquin telleria <joaquintelleria@gmail.com>
2023-01-11 15:22:29 +0100

    .

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,13 @@ import express, { Express, Request, Response } from 'express';
 import path from 'path';
 import session from 'express-session';
 
-console.log("Welcome to the TestProjectAlpha Alpha");
+let validPasswords: Array<[string, string]> = [
+    ["admin", "admin"]
+];
+
+let validAuthTokens: Array<string> = [
+
+];
 
 const app = express();
 
@@ -11,9 +17,8 @@ app.set('trust proxy', 1)
 
 app.use(session({
     secret: 'teddy bear', 
-    cookie: { maxAge: 60000 }
+    cookie: { maxAge: 10000 }
 }))
-
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -22,17 +27,13 @@ app.use(bodyParser.json())
 app.get('/', (req: Request, res: Response) => 
 {
     let session = req.session;
-    console.log("Session id: ", session.id);
-
-
-    let usersA = JSON.parse('{ "username":"admin", "password":"admin"}');
-    console.log(usersA.username);
-
-    if(session.id){
+    
+    if (validAuthTokens.includes(session.id)){
         res.sendFile(path.resolve(__dirname, 'index.html'));
     } else {
-        res.sendFile(path.resolve(__dirname, 'login.html'));
+        res.redirect('/login');
     }
+
 });
 
 app.get('/login', (req: Request, res: Response) => {
@@ -45,11 +46,12 @@ app.post('/login', (req: Request, res: Response) => {
 
   if (username == "admin" && password=="admin")
   {
-  res.redirect('/');
+    validAuthTokens.push(req.session.id);
+    res.redirect('/');
   } 
   else 
   {
-  res.send("Access Denied");
+    res.send("Access Denied");
   }
 });
 
