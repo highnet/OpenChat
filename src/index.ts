f335c7706a7e8e7317d408db8602cef0021bc317
joaquin telleria <joaquintelleria@gmail.com>
2023-01-12 14:43:23 +0100

    .

--- src/index.ts
+++ src/index.ts
@@ -10,21 +10,15 @@ import fs from "fs";
 
 // socket.io
 
-let validPasswords: Array<[string, string]> = [
-    ["admin", "admin"]
-];
-
-let validAuthTokens: Array<string> = [
-
-];
-
 const app = express();
 
 app.set('trust proxy', 1)
 
 app.use(session({
     secret: 'teddy bear', 
-    cookie: { maxAge: 10000 }
+    cookie: { maxAge: 1000000 },
+    resave: false,
+    saveUninitialized: true
 }))
 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -33,15 +27,33 @@ app.use(bodyParser.json())
 
 let users = JSON.parse(fs.readFileSync('users.json','utf8'));
 
+let auths: string[] = [];
+
 app.get('/', (req: Request, res: Response) => 
 {
-    let session = req.session;
-    
-    if (validAuthTokens.includes(session.id)){
-        res.sendFile(path.resolve(__dirname, '../views/index.html'));
-    } else {
-       res.redirect('/login');
+  let session = req.session;
+  let validAuth: boolean = false;
+  
+  console.log("session ID:", session.id);
+
+  // auths = JSON.parse(fs.readFileSync('auths.json','utf8'));
+
+  for(let auth of auths){
+    console.log(auth);
+    if (session.id == auth){
+      validAuth = true;
+      break;
     }
+  }
+
+  if (validAuth){
+    console.log("Valid Authentication");
+    res.sendFile(path.resolve(__dirname, '../views/index.html'));
+  } else {
+    console.log("Invalid Authentication");
+    console.log("Redirecting to login");
+    res.redirect('/login');
+  }
 
 });
 
@@ -53,17 +65,22 @@ app.get('/login', (req: Request, res: Response) => {
 app.post('/login', (req: Request, res: Response) => {
   let usernameInput = req.body.username;
   let passwordInput = req.body.password;
+  let session = req.session;
 
-  let validInput: boolean = false;
+  console.log("session ID:", session.id);
+
+  let validCredentials: boolean = false;
   for(let user of users){
-    if (usernameInput == user.username && passwordInput == user.password ){
-      validInput = true;
+    if (usernameInput == user.username && passwordInput == user.password){
+      validCredentials = true;
       break;
     }
   }
 
-  if (validInput){
-    validAuthTokens.push(req.session.id);
+  if (validCredentials){
+    auths.push(session.id);
+    fs.writeFileSync('auths.json', JSON.stringify(auths),'utf8');
+    console.log("Redirecting to /");
     res.redirect('/');
   } else {
     res.redirect('/login?err=LOGIN_INCORRECT')
@@ -74,4 +91,3 @@ app.use('/static', express.static(path.join(__dirname, '..', 'static')));
 app.listen(3000, () => {
     console.log('The application is listening on port 3000!');
 })
-
