99685836018d961ceb5c937f9faeef0f7d9c98e7
joaquin telleria <joaquintelleria@gmail.com>
2023-01-19 23:08:33 +0100

    .

--- static/chatMessage.js
+++ static/chatMessage.js
@@ -4,7 +4,7 @@ function generateChatMessageHTML(messageData) {
     let component = `
     <div class="container chat-message ${other ? "chat-message-other":"chat-message-mine"}">
         <span class="container chat-message-chat-bubble"></span>
-        <p class="container chat-message-chat-nickname">
+        <p class="container chat-message-chat-nickname ${other ? "chat-message-chat-nickname-other" : "chat-message-chat-nickname-mine"}">
             ${messageData.nickname + ':'} 
         </p>
         <p class="container chat-message-chat-text">
