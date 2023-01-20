73b8e6b2c2791be22984af0804be3752168be0b7
joaquin telleria <joaquintelleria@gmail.com>
2023-01-20 10:28:01 +0100

    .

--- static/chatRoom.js
+++ static/chatRoom.js
@@ -4,12 +4,11 @@ let socket = io();
 let chatterClient = new Chatter();
 
 function convertMessageDataToHTMLComponent(messageData) {
-
-    let other = messageData.uuid == chatterClient.uuid ? false:true;
+    let messageBelongsToClient = messageData.uuid == chatterClient.uuid ? false : true;
     let component = `
-    <div class="container chat-message ${other ? "chat-message-other":"chat-message-mine"}">
+    <div class="container chat-message ${messageBelongsToClient ? "chat-message-other":"chat-message-mine"}">
         <span class="container chat-message-chat-bubble"></span>
-        <p class="container chat-message-chat-nickname ${other ? "chat-message-chat-nickname-other" : "chat-message-chat-nickname-mine"}">
+        <p class="container chat-message-chat-nickname ${messageBelongsToClient ? "chat-message-chat-nickname-other" : "chat-message-chat-nickname-mine"}">
             ${messageData.nickname + ':'} 
         </p>
         <p class="container chat-message-chat-text">
