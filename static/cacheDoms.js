ece84ccbac84e3382e3812109969dd84bd0d2c97
joaquin telleria <joaquintelleria@gmail.com>
2023-01-20 12:30:14 +0100

    .

--- static/cacheDoms.js
+++ static/cacheDoms.js
@@ -1,5 +1,4 @@
 let messageInputForm = document.getElementById("messageInput");
 let messageInputField = document.getElementById("messageInputField");
-let nicknameInputField = document.getElementById("nicknameInputField");
 let chatMessages = document.getElementById("chatMessages");
 let chatClientsList = document.getElementById("chatClients");
