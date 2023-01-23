bf9d754d2ca4bbaa8a67b809f2aa46e22cbe1818
joaquin telleria <joaquintelleria@gmail.com>
2023-01-23 13:51:47 +0100

    .

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+//#region Imports
+
 import bodyParser, { json } from 'body-parser';
 import express, { Express, Request, Response } from 'express';
 import path from 'path';
@@ -7,8 +9,16 @@ import http from 'http';
 import { Server } from "socket.io";
 import { Users } from './users';
 
-// https://greensock.com/
+//#endregion Imports
 
+
+//#region Enums
+
+/*
+  ServerEmissions describes the different types of
+  emissions that can be emited by the server to the client
+  the client has an equivalent definition of ServerEmissions
+*/
 enum ServerEmissions{
   YOU_LOGGED_IN = "you logged in",
   A_CLIENT_LOGGED_IN = "a client logged in",
@@ -16,30 +26,53 @@ enum ServerEmissions{
   CHAT_MESSAGE = "chat message"
 }
 
+/*
+  ServerEvents describes the different types of server events
+*/
 enum ServerEvents{
   CONNECTION = "connection",
   DISCONNECT = "disconnect"
 }
 
-const app = express();
-app.set('trust proxy', 1)
+//#endregion Enums
 
-app.use(session({
-    secret: 'teddy bear', 
-    cookie: { maxAge: 100000000000000 },
+
+//#region App Setup
+
+const app = express(); // Create an express application
+
+app.set('trust proxy', 1); // Enable reverse proxy support
+
+app.use(
+  session({
+    secret: 'teddy bear',
+    cookie: { maxAge: 100000000000000 }, 
     resave: false,
     saveUninitialized: true
-}))
+})); // Define session configuration
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false })); // enable the middleware for parsing bodies
+app.use(bodyParser.json()); // enable the middleware for parsing json
 app.use('/static', express.static(path.join(__dirname, '..', 'static')));
 
-let validLogins = JSON.parse(fs.readFileSync('logins.json','utf8'));
-let validAuths: string[] = [];
-let activeUsers = new Users();
+//#endregion App Setup
+
+//#region Global
+
+let validLogins = JSON.parse(
+  fs.readFileSync(
+    'logins.json',
+    'utf8'
+)); // cache the JSON file containing the list of valid logins
+let validAuths: string[] = []; // initialize the list used to cache valid auth tokens
+let connectedUsers = new Users(); // initialize the object used to cache a list of users connected to the server
 let server = http.createServer(app);
 let io = new Server(server);
+
+//#endregion Global
+
+
+//#region Post/Get
 
 app.get('/', (req: Request, res: Response) => 
 {
@@ -88,17 +121,20 @@ app.post('/login', (req: Request, res: Response) => {
 
 });
 
+//#endregion Post/Get
+
+//#region Socket
 io.on(ServerEvents.CONNECTION, (socket) => {
   
-  let newUser = activeUsers.GenerateNewUser();
+  let connectedUser = connectedUsers.GenerateNewUser();
   
-  socket.emit(ServerEmissions.YOU_LOGGED_IN, newUser.Uuid, newUser.Nickname, activeUsers);
-  socket.broadcast.emit(ServerEmissions.A_CLIENT_LOGGED_IN, activeUsers);
+  socket.emit(ServerEmissions.YOU_LOGGED_IN, connectedUser.Uuid, connectedUser.Nickname, connectedUsers);
+  socket.broadcast.emit(ServerEmissions.A_CLIENT_LOGGED_IN, connectedUsers);
   
   socket.on(ServerEvents.DISCONNECT, () => {
 
-    activeUsers.RemoveOldUser(newUser);
-    socket.broadcast.emit(ServerEmissions.A_CLIENT_LOGGED_OUT, activeUsers);
+    connectedUsers.RemoveOldUser(connectedUser);
+    socket.broadcast.emit(ServerEmissions.A_CLIENT_LOGGED_OUT, connectedUsers);
 
   });
 
@@ -111,6 +147,4 @@ io.on(ServerEvents.CONNECTION, (socket) => {
 server.listen(3000, () => {
     console.log('The application is listening on port 3000!');
 })
-
-
-
+//#endregion Socket
