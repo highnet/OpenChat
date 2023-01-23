4574a0c25aaf8d5391ebe7a4c4a73706498e8f6e
joaquin telleria <joaquintelleria@gmail.com>
2023-01-23 15:58:58 +0100

    .

--- src/user.ts
+++ src/user.ts
@@ -1,4 +1,5 @@
 import { v4 as uuidv4 } from 'uuid';
+import fs from "fs";
 
 export class User{
 
@@ -12,13 +13,20 @@ export class User{
 
     private generateRandomNickname(): string{
         let result = "";
-        const adjectives = ["Dopey", "Doc", "Sneezy", "Bashful", "Sleepy", "Grumpy", "Happy"];
-        const subjectives = ["Car", "Dog", "House", "Moon", "Water", "Table", "Trouble"];
-        let randomAdjective = adjectives[Math.floor(Math.random()*adjectives.length)];
-        let randomSubjective = subjectives[Math.floor(Math.random()*subjectives.length)];
+        let nicknames = JSON.parse(
+         fs.readFileSync(
+            'nicknames.json',
+            'utf8'
+        ));
+        const firstNames = nicknames.firstNames;
+        const lastNames = nicknames.lastNames;
+        let randomAdjective = firstNames[Math.floor(Math.random()*firstNames.length)];
+        let randomSubjective = lastNames[Math.floor(Math.random()*lastNames.length)];
         result += randomAdjective;
         result += randomSubjective;
         result += Math.floor(Math.random() * 90 + 10); 
+
+
         return result; 
     }
 
