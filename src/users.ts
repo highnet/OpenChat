04e1ee69771f0154f95d6c274dc25c4916298601
joaquin telleria <joaquintelleria@gmail.com>
2023-01-24 10:14:27 +0100

    .

--- src/users.ts
+++ src/users.ts
@@ -1,34 +1,31 @@
-import { v4 as uuidv4 } from 'uuid';
-import { User } from './user';
+import { v4 as uuidv4 } from "uuid";
+import { User } from "./user";
 
 export class Users {
+  private _Users: Array<User>;
 
-    private _Users: Array<User>;
+  constructor() {
+    this._Users = [];
+  }
 
-    constructor(){
-        this._Users = [];
+  public get Users(): Array<User> {
+    return this._Users;
+  }
+
+  public set Users(value: Array<User>) {
+    this._Users = value;
+  }
+
+  public GenerateNewUser(): User {
+    let newUser = new User();
+    this._Users.push(newUser);
+    return newUser;
+  }
+
+  public RemoveOldUser(user: User): void {
+    let index = this._Users.indexOf(user);
+    if (index !== -1) {
+      this._Users.splice(index, 1);
     }
-
-    public get Users(): Array<User>{
-        return this._Users;
-    }
-
-    public set Users(value: Array<User>){
-        this._Users = value;
-    }
-
-
-    public GenerateNewUser(): User {
-        let newUser = new User();
-        this._Users.push(newUser);
-        return newUser;
-    }
-
-    public RemoveOldUser(user:User):void {
-        let index = this._Users.indexOf(user);
-        if (index !== -1){
-        this._Users.splice(index, 1);
-        }
-    }
-
+  }
 }
