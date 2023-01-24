78f7e7f2bc5b05e78b2e5337dbfefcf7f0cc002d
joaquin telleria <joaquintelleria@gmail.com>
2023-01-24 10:14:09 +0100

    .

--- src/user.ts
+++ src/user.ts
@@ -1,46 +1,42 @@
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import fs from "fs";
 
-export class User{
+export class User {
+  private _Uuid: string;
+  private _Nickname: string;
 
-    private _Uuid: string;
-    private _Nickname: string;
+  constructor() {
+    this._Uuid = uuidv4();
+    this._Nickname = this.generateRandomNickname();
+  }
 
-    constructor(){
-        this._Uuid = uuidv4();
-        this._Nickname = this.generateRandomNickname();
-    }
+  private generateRandomNickname(): string {
+    let result = "";
+    let nicknames = JSON.parse(fs.readFileSync("nicknames.json", "utf8"));
+    const firstNames = nicknames.firstNames;
+    const lastNames = nicknames.lastNames;
+    let randomFirstName =
+      firstNames[Math.floor(Math.random() * firstNames.length)];
+    let randomSecondName =
+      lastNames[Math.floor(Math.random() * lastNames.length)];
+    result +=
+      randomFirstName + randomSecondName + Math.floor(Math.random() * 90 + 10);
+    return result;
+  }
 
-    private generateRandomNickname(): string{
-        let result = "";
-        let nicknames = JSON.parse(
-         fs.readFileSync(
-            'nicknames.json',
-            'utf8'
-        ));
-        const firstNames = nicknames.firstNames;
-        const lastNames = nicknames.lastNames;
-        let randomFirstName = firstNames[Math.floor(Math.random()*firstNames.length)];
-        let randomSecondName = lastNames[Math.floor(Math.random()*lastNames.length)];
-        result += randomFirstName +
-            randomSecondName +
-            Math.floor(Math.random() * 90 + 10);
-        return result; 
-    }
+  public get Uuid(): string {
+    return this._Uuid;
+  }
 
-    public get Uuid(): string{
-        return this._Uuid;
-    }
+  public set Uuid(value: string) {
+    this._Uuid = value;
+  }
 
-    public set Uuid(value: string){
-        this._Uuid = value;
-    }
+  public get Nickname(): string {
+    return this._Nickname;
+  }
 
-    public get Nickname(): string{
-        return this._Nickname;
-    }
-
-    public set Nickname(value: string){
-        this._Nickname = value;
-    }
+  public set Nickname(value: string) {
+    this._Nickname = value;
+  }
 }
