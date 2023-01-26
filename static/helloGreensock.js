f153d8c457e1ce6038d98eea4d159022e0f5506f
joaquin telleria <joaquintelleria@gmail.com>
2023-01-26 15:29:44 +0100

    ,

--- static/helloGreensock.js
+++ static/helloGreensock.js
@@ -3,7 +3,7 @@ let tl = gsap.timeline({ repeat: -1, repeatDelay: 1, yoyo: true });
 
 tl.to(".decoration-box-green", { rotation: 360 });
 tl.to(".decoration-box-purple", { rotation: 360 });
-tl.to(".ecoration-box-orange", { rotation: 360 });
+tl.to(".decoration-box-orange", { rotation: 360 });
 tl.play();
 
 console.log("test");
