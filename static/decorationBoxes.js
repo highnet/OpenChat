c0f81794c922730235de7ff7a045cb2ff1b0c161
Joaquin <joaquinteleria@gmail.com>
2023-01-30 13:06:45 +0100

    .

---
+++ static/decorationBoxes.js
@@ -0,0 +1,21 @@
+// create a timeline
+let tl = gsap.timeline({ repeat: -1, yoyo: true });
+
+tl.to(".decoration-box-1", { rotation: 360 });
+tl.to(".decoration-box-2", { rotation: 360 });
+tl.to(".decoration-box-3", { rotation: 360 });
+tl.to(".decoration-box-4", { rotation: 360 });
+tl.to(".decoration-box-5", { rotation: 360 });
+tl.to(".decoration-box-6", { rotation: 360 });
+tl.to(".decoration-box-7", { rotation: 360 });
+tl.to(".decoration-box-8", { rotation: 360 });
+tl.to(".decoration-box-9", { rotation: 360 });
+tl.to(".decoration-box-10", { rotation: 360 });
+tl.to(".decoration-box-11", { rotation: 360 });
+tl.to(".decoration-box-12", { rotation: 360 });
+tl.to(".decoration-box-13", { rotation: 360 });
+tl.to(".decoration-box-14", { rotation: 360 });
+tl.to(".decoration-box-15", { rotation: 360 });
+tl.to(".decoration-box-16", { rotation: 360 });
+
+tl.play();
